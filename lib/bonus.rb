9f1bb2611656201be29778717ea00c6a294bc1df
Monica Lisa Reyes <mlisareyes@gmail.com>
2019-08-14 04:33:13 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,9 +20,8 @@ def bonus
    }
   }
 
-  bonus[:montague][:hero][:status] = "dead"
-  bonus[:capulet][:heroine][:status] = "dead"
-  return bonus
+  epic_tragedy[:montague][:hero][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
 
   
 
